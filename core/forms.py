7508ec3a1c21ae94230e7a1299eff461738714e0
SayemImran <ik262142@gmail.com>
2025-12-31 18:16:50 +0600

    style applied

--- core/forms.py
+++ core/forms.py
@@ -224,7 +224,31 @@ class CustomPasswordChangeForm(PasswordChangeForm):
             "placeholder": "Confirm New Password"
         })
 
+# class CustomPasswordResetForm(PasswordResetForm):
+#     pass
+# class CustomPasswordResetConfirmForm(SetPasswordForm):
+#     pass
+TAILWIND_INPUT_CLASSES = (
+    "w-full px-4 py-2 border border-gray-300 rounded-lg "
+    "focus:outline-none focus:ring-2 focus:ring-blue-500 "
+    "focus:border-blue-500"
+)
+
 class CustomPasswordResetForm(PasswordResetForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": TAILWIND_INPUT_CLASSES,
+                "placeholder": field.label
+            })
+
+
 class CustomPasswordResetConfirmForm(SetPasswordForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": TAILWIND_INPUT_CLASSES,
+                "placeholder": field.label
+            })
